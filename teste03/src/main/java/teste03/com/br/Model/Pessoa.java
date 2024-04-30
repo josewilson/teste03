1e4ca6c654a8987e78604c416d3677d0226dc1ed
jose wilson <jwasouza73@gmail.com>
2024-04-30 12:21:37 -0300

    Efetuando alterações na classe Pessoa

--- teste03/src/main/java/teste03/com/br/Model/Pessoa.java
+++ teste03/src/main/java/teste03/com/br/Model/Pessoa.java
@@ -2,25 +2,17 @@ package teste03.com.br.Model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Date;
 import java.util.List;
 
 @Data
 @Entity
 public class Pessoa {
-    @Setter
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Getter
-    @Setter
     private String nomeCompleto;
-    @Setter
-    @Getter
     private Date dataNascimento;
 
     @OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL, orphanRemoval = true)
